7b3907983b0e4b8ecdfcf95142b10a2256ae4a6b
Adrian Białobrodzki <93153245+abialobrodzki@users.noreply.github.com>
2021-12-03 01:33:25 +0100

    New version my SQL files
    SQL - new files included:
    - fixed prework files
    - new sakila testing database

--- prework - nauczyciele i studenci.sql
+++ prework - nauczyciele i studenci.sql
@@ -1,17 +1,18 @@
--- show tables; /* -- komentarz wyłączający wiersz */
--- describe Classes; /* zawartość tabeli Classes - wielkość liter ma znaczenie :) */
--- select * from Classes; /* pobranie wszystkich danych dla tabeli Classes */
--- describe Students; /* zawartość tabeli Students */
-select *
-from Students; /* ad.1 wszystkie dane studentów z tabeli Students */
-select * from Students
-where name like 'De%' and class_id = 11; /* szuka imienia wg wzorca zaczynającego się na frazę "De" i należy do klasy o id równym 11 */
-select * from Students
-where class_id = 1 or class_id = 2; /* studenci z klas o id=1 oraz id=2  */
-select * from Students
-order by surname asc, name asc; /* sortowanie wg nazwisk i imion - rosnąco */
-select name, surname
-from Students; /* ad.2 imiona i nazwiska studentów z tabeli Students */
+-- !!! dobrą praktyką jest stosowanie WIELKICH LITER w komendach !!!
+-- SHOW tables; /* -- komentarz wyłączający wiersz */
+-- DESCRIBE Classes; /* zawartość tabeli Classes - wielkość liter ma znaczenie :) */
+-- SELECT * FROM Classes; /* pobranie wszystkich danych dla tabeli Classes */
+-- DESCRIBE Students; /* zawartość tabeli Students */
+SELECT *
+FROM Students; /* ad.1 wszystkie dane studentów z tabeli Students */
+SELECT * FROM Students
+WHERE name LIKE 'De%' AND class_id = 11; /* szuka imienia wg wzorca zaczynającego się na frazę "De" i należy do klasy o id równym 11 */
+SELECT * FROM Students
+WHERE class_id = 1 OR class_id = 2; /* studenci z klas o id=1 oraz id=2  */
+SELECT * FROM Students
+ORDER BY surname ASC, name ASC; /* sortowanie wg nazwisk i imion - rosnąco */
+SELECT name, surname
+FROM Students; /* ad.2 imiona i nazwiska studentów z tabeli Students */
 -- describe Teachers; /* zawartość tabeli Teachers */
-select *
-from Teachers; /* ad.3 wszystkie dane nauczycieli z tabeli Teachers */
+SELECT *
+FROM Teachers; /* ad.3 wszystkie dane nauczycieli z tabeli Teachers */
---
+++ sakila.sql
@@ -0,0 +1,6 @@
+USE sakila;
+-- polecenia testujące --
+SHOW FULL tables; -- row 23 in table
+SELECT COUNT(*) FROM film; -- sprawdzenie count 1000
+SELECT COUNT(*) FROM film_text; -- sprawdzenie count 1000
+
