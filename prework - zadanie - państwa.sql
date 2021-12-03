b20ea48bcaccc5738d84dfd4cbc4e37a21efca3e
Adrian Białobrodzki <93153245+abialobrodzki@users.noreply.github.com>
2021-12-03 01:33:25 +0100

    New version my SQL files
    SQL - new files included:
    - fixed prework files
    - new sakila testing database

--- prework - zadanie - państwa.sql
+++ prework - zadanie - państwa.sql
@@ -1,16 +1,23 @@
--- show tables;
--- describe country;
-select * from country
-where Name like 'Po%' order by Name asc; /* ad.1 wszystkie informacje o krajach zaczynających się od frazy "Po" */
-select Code, Name from country
-where Continent = "Europe" order by Name asc; /* ad.2 lista krajów w Europie: skrót i nazwa */
-select Name, Continent, HeadOfState from country
-where HeadOfState like 'K%' or HeadOfState is null; /* ad.6 lista z nazwą kraju, kontynentem, władcą kraju - nie ma go lub zaczyna się na "K" */
--- describe city;
-select * from city
-where CountryCode = "ARG" and Population > 190000 order by Population desc; /* ad.4 wszystkie informacje o miasta w Argentynie powyżej 190 tys. mieszkańców */
-select Name from city
-where Population < 10000 order by Population asc; /* ad.5 lista miast z populacją poniżej 10 tys. */
--- describe countrylanguage;
-select CountryCode from countrylanguage
-where Language = "Polish" and IsOfficial = "F" order by CountryCode asc; /* ad.3 kod kraju w którym polski występuje, ale nie jest oficjalnym językiem */
+-- !!! dobrą praktyką jest stosowanie WIELKICH LITER w komendach !!!
+-- SHOW tables;
+-- DESCRIBE country;
+-- SELECT * FROM country WHERE Name LIKE 'Po%';
+SELECT * FROM country
+WHERE Name LIKE 'Po%' ORDER BY Name ASC; /* ad.1 wszystkie informacje o krajach zaczynających się od frazy "Po" */
+-- SELECT Code, Name FROM country WHERE Continent = "Europe";
+SELECT Code, Name FROM country
+WHERE Continent = "Europe" ORDER BY Name ASC; /* ad.2 lista krajów w Europie: skrót i nazwa */
+-- SELECT Name, Continent, HeadOfState FROM country WHERE HeadOfState LIKE 'K%' OR HeadOfState IS NULL;
+SELECT Name, Continent, HeadOfState FROM country
+WHERE HeadOfState LIKE 'K%' OR HeadOfState IS NULL ORDER BY Name; /* ad.6 lista z nazwą kraju, kontynentem, władcą kraju - nie ma go lub zaczyna się na "K" */
+-- DESCRIBE city;
+-- SELECT * FROM city WHERE CountryCode = "ARG" AND Population > 190000 ORDER BY Population DESC;
+SELECT * FROM city
+WHERE CountryCode = "ARG" AND Population > 190000 ORDER BY Population DESC; /* ad.4 wszystkie informacje o miasta w Argentynie powyżej 190 tys. mieszkańców */
+-- SELECT Name FROM city WHERE Population < 10000 ORDER BY Population ASC;
+SELECT Name FROM city
+WHERE Population < 10000 ORDER BY Population ASC; /* ad.5 lista miast z populacją poniżej 10 tys. */
+-- DESCRIBE countrylanguage;
+-- SELECT CountryCode FROM countrylanguage WHERE Language = "Polish" and IsOfficial = "F";
+SELECT CountryCode FROM countrylanguage
+WHERE Language = "Polish" AND IsOfficial = "F" ORDER BY CountryCode ASC; /* ad.3 kod kraju w którym polski występuje, ale nie jest oficjalnym językiem */
